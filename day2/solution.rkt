4d67466567f4c40a363853d087576bed174a70ba
Levon Vardevanyan <levon.vardevanyan@n-able.com>
2025-12-02 11:33:57 +0000

    day two

---
+++ day2/solution.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+(require threading)
+
+(define ranges
+  (~> "input.txt"
+      file->string
+      string-trim
+      (string-split ",")))
+
+(define (unpack r)
+  (map string->number (string-split r "-")))
+
+(define (invalid-id? id)
+  (define s (number->string id))
+  (define len (string-length s))
+  (and (even? (string-length s))
+       (let ([lhs (substring s 0 (/ len 2))]
+             [rhs (substring s (/ len 2))])
+         (string=? lhs rhs))))
+
+(define (divisible? n m)
+  (zero? (remainder n m)))
+
+(define (all-equal? lst)
+  (for/and ([x lst][y (rest lst)])
+    (equal? x y)))
+
+(define (invalid-id-v2? id)
+  (define s (number->string id))
+  (define len (string-length s))
+  (for/or ([sz (in-inclusive-range 1 (quotient len 2))]
+           #:when (divisible? len sz))
+    (all-equal?
+     (for/list ([i (in-range 0 len sz)])
+       (substring s i (+ i sz))))))
+
+(define (solve pred?)
+  (for/sum ([r ranges])
+    (match-define (list from to) (unpack r))
+    (for/sum ([i (in-inclusive-range from to)]
+              #:when (pred? i))
+      i)))
+
+(solve invalid-id?)
+(solve invalid-id-v2?)
