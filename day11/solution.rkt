1373da363a5060294ecd7064c3b9473d0b5399bc
Levon Vardevanyan <levon.vardevanyan@n-able.com>
2025-12-11 22:28:55 +0000

    day eleven

---
+++ day11/solution.rkt
@@ -0,0 +1,44 @@
+#lang racket
+
+(require memo)
+(require threading)
+
+(define (parse-line line)
+  (match-define (list from to) (string-split line ":"))
+  (cons from (string-split to)))
+
+(define dag
+  (~> "input.txt"
+      file->lines
+      (map parse-line _)
+      make-hash))
+
+(define (draw-graph dag)
+  (define (dot-gen)
+    (define nodes
+      (for/list ([(k v) (in-hash dag)])
+        (format "~a -> { ~a }" k (string-join v " "))))
+    (format "digraph {\n~a}" (string-join nodes ";\n")))
+
+  (define dot.exe "/opt/homebrew/bin/dot")
+  (system (format "echo '~a' | ~a -Tsvg -o graph.svg" (dot-gen) dot.exe)))
+
+(draw-graph dag)
+
+(define/memoize (how-many? from to)
+  (if (string=? from to)
+      1
+      (let ([children (hash-ref dag from empty)])
+        (for/sum ([child children]) (how-many? child to)))))
+
+;; part 1
+(how-many? "you" "out")
+
+;; part 2
+(+
+ (* (how-many? "svr" "dac")
+    (how-many? "dac" "fft")
+    (how-many? "fft" "out"))
+ (* (how-many? "svr" "fft")
+    (how-many? "fft" "dac")
+    (how-many? "dac" "out")))
